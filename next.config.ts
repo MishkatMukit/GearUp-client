8163c75a532c685ac4310ddc65e014e3019bded7
MishkatMukit <mahabubmishkat8@gmail.com>
2026-08-02 04:22:12 +0600

    chore: remove unused remote pattern for example.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "example.com",
-      },
-      {
-        protocol: "https",
         hostname: "i.ibb.co.com",
       },
       {
